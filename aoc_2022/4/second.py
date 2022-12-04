526d9d030a8861de29a37f243172daf46e7db52b
b0n3sh <vanper77@gmail.com>
2022-12-04 17:38:18 +0100

    4th day

---
+++ aoc_2022/4/second.py
@@ -0,0 +1,14 @@
+#!venv/bin/python3
+
+score = 0
+with open('input', 'r') as c:
+	lines = c.read().splitlines()
+	for line in lines:
+		first_elf, second_elf = line.split(',')
+		first_elf_first_zone, first_elf_second_zone = map(lambda x: int(x), first_elf.split('-'))
+		second_elf_first_zone, second_elf_second_zone = map(lambda x: int(x), second_elf.split('-'))
+
+		if (first_elf_first_zone <= second_elf_first_zone and first_elf_second_zone >= second_elf_first_zone) or (second_elf_first_zone <= first_elf_first_zone and second_elf_second_zone >= first_elf_first_zone):
+			score+=1
+
+print(score)
